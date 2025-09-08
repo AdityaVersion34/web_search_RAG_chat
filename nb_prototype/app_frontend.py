074af5a8f8c817bdd821c3e5810601f50bb5e5ad
AdityaVersion34 <112605343+AdityaVersion34@users.noreply.github.com>
2025-09-07 23:56:48 -0500

    tweaking text yet again

--- nb_prototype/app_frontend.py
+++ nb_prototype/app_frontend.py
@@ -7,8 +7,9 @@ st.title("Web-Search RAG Chatbot")
 st.write("""This app is an LLM chatbot that uses RAG along with web search 
          results to provide up-to-date and relevant information. 
          This is similar to modern chatbot applications like Perplexity.
-         \nBuilt primarily with LangGraph and Streamlit.\nRead more about this project at https://adityavenkatesh.com\n
-         Built by Aditya Venkatesh""")
+         \nBuilt primarily with LangGraph and Streamlit.
+         \nRead more about this project at https://adityavenkatesh.com
+         \nBuilt by Aditya Venkatesh""")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
